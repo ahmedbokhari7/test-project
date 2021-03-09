ef18f28157c45fff020e84041bef54932fb741ae
Ahmed Bokhari <ahmedbokhari7@gmail.com>
2021-03-09 15:14:39 +0500

    data set is working properly

--- src/main/java/com/ahmed/enums/ConfigPath.java
+++ src/main/java/com/ahmed/enums/ConfigPath.java
@@ -4,6 +4,7 @@ public class ConfigPath {
 	private static final String customer = System.getProperty("customer") != null ? System.getProperty("customer")
 			: "ers-std";
 //	private static final boolean local_run = Boolean.valueOf(System.getProperty("local.run"));
+	
 	private static final boolean local_run = true;
 
 	public static final String DEFAULT_TEST_DATA = local_run ? "src/test/resources/customer/"
